9e6d138ba0768327a9f0b0d1063cc5d8f19e1baa
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-05-05 14:22:52 +0100

    migration fixes

--- gomi/migration.go
+++ gomi/migration.go
@@ -2,7 +2,6 @@ package gomi
 
 import (
 	"labix.org/v2/mgo"
-	"labix.org/v2/mgo/bson"
 )
 
 var migrationsDir string = "migrations"
@@ -15,12 +14,12 @@ type Migration struct {
 
 type Migrator struct {
 	Migrations *mgo.Collection
-    Structures *mgo.Collection
+	Structures *mgo.Collection
 }
 
 func NewMigrator(migrations *mgo.Collection, structures *mgo.Collection) *Migrator {
 	return &Migrator{
-        Migrations: migrations, 
-        Structures: structures
-    }
+		Migrations: migrations,
+		Structures: structures,
+	}
 }
